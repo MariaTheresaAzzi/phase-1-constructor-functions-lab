252dcae6d2f021f16c8d1e36d5bcff6877c39e8b
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-09-08 13:30:51 +0300

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+//Scooter with year, color, and model
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+//Driver with name, age, and experience
+function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+//PickupLocation with address and city
+function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+}
